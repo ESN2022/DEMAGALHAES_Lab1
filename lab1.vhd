cedddb887b2e1003ea007ed1b35e5199a1603369
Raphael DE MAGALHAES <rademagalh>
2023-01-03 17:31:38 +0100

    fin de seance

--- lab1.vhd
+++ lab1.vhd
@@ -6,21 +6,29 @@ use ieee.numeric_std.all;
 entity lab1 is
 	port(
 		clk : in std_logic := '0';
-		reset_n : in std_logic :='0');
+		reset_n : in std_logic :='0';
+		pio_0 : out std_logic_vector(7 downto 0) :="00000000");
 end entity lab1;
 
 architecture rit of lab1 is
 	component lab1_sys is
-		  port (
-				clk_clk       : in std_logic := 'X'; -- clk
-				reset_reset_n : in std_logic := 'X'  -- reset_n
-		  );
-	end component lab1_sys;
+        port (
+            clk_clk                          : in  std_logic                    := 'X'; -- clk
+            reset_reset_n                    : in  std_logic                    := 'X'; -- reset_n
+            pio_0_external_connection_export : out std_logic_vector(7 downto 0)         -- export
+        );
+    end component lab1_sys;
 
 begin
 	u0 : component lab1_sys
         port map (
-            clk_clk       => clk,      --   clk.clk
-            reset_reset_n => reset_n -- reset.reset_n
+            clk_clk                          => clk,      -- clk.clk
+            reset_reset_n                    => reset_n,  -- reset.reset_n
+            pio_0_external_connection_export => pio_0     -- pio_0_external_connection.export
         );
 end architecture;
+
+
+    
+
+    
--- software/app/main.c
+++ software/app/main.c
@@ -1,6 +1,9 @@
-#include "sys/alt_stdio.h"
+#include “system.h”
+#include “altera_avalon_pio_regs.h"
 
 int main(){
-	alt_printf("Hello World");
-	return 0;
-}
+	while(1){
+		IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_BASE,0x01);
+		IOWR_ALTERA_AVALON_PIO_DATA(PIO_BASE,0xff);
+		return 0;
+	}
