806ca570d3874530170ac54c8bdb0ac54862dd1d
Raphaël DE MAGALHAES <rphmagalhaes@gmail.com>
2023-01-09 17:49:44 +0100

    Update du vhd, main.c a tester, ajout du bouton dans le qsys

--- software/app/main.c
+++ software/app/main.c
@@ -1,9 +1,29 @@
-#include “system.h”
-#include “altera_avalon_pio_regs.h"
+#include "system.h"
+#include "altera_avalon_pio_regs.h"
+#include "sys/alt_stdio.h"
+#include "alt_types.h"
+#include "io.h"
+#include "unistd.h"
 
 int main(){
+
+	int LedAddr = 0x01;
+	IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE, LedAddr);
+	
 	while(1){
-		IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_BASE,0x01);
-		IOWR_ALTERA_AVALON_PIO_DATA(PIO_BASE,0xff);
-		return 0;
+		while (LedAddr != 0x80)
+		{
+			LedAddr = LedAddr << 1; // Décalage led par led
+			IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,LedAddr);
+			usleep(10000);
+        }
+        while (LedAddr != 0x01)
+		{
+			LedAddr = LedAddr >> 1;
+			IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,LedAddr);
+			usleep(10000);
+		}
 	}
+		
+	return 0;		
+}
