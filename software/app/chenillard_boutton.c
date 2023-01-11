77fe89533ba19c49914442906f8c0739a8cbd14c
Raphael DE MAGALHAES <rademagalh>
2023-01-11 08:57:16 +0100

    chenillard simple et boutton polling fonctionnel

--- software/app/chenillard_boutton.c
+++ software/app/chenillard_boutton.c
@@ -6,21 +6,30 @@
 #include "unistd.h"
 
 int main(){
-	
+	int led = 0x01;
+	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
 	while(1){
-		if (IOWR_ALTERA_AVALON_PIO_DATA(BUTTON_BASE) == 0b10){
-			for (int led = 0; led < 7; led++)
+		if (IORD_ALTERA_AVALON_PIO_DATA(BUTTONS_BASE) == 0b10){
+			//alt_printf("i+ \n");
+			for (int i = 0; i < 7; i++)
 			{
-				IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,1<<led);
-				usleep(10000);
+				//alt_printf("i: %x\n",i);
+				led = led<<1;
+				IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
+				//alt_printf("led<<1: %x\n",led);
+				usleep(100000);
 			}
-			for (int led = 7; led > 0 ; led--)
+			//alt_printf("i- \n");
+			for (int i = 7; i > 0 ; i--)
 			{
-				IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,1<<led);
-				usleep(10000);
+				//alt_printf("i: %x\n",i);
+				led = led>>1;
+				IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
+				//alt_printf("led<<1: %x\n",led);
+				usleep(100000);
 			}
 		}
-		else IOWR_ALTERA_AVALON_PIO_DATA(LED_BASE,0);
+		else IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,0);
 	}
 		
 	return 0;		
--- software/app/chenillard_simple.c
+++ software/app/chenillard_simple.c
@@ -6,17 +6,27 @@
 #include "unistd.h"
 
 int main(){
-
+	int led = 0x01;
+	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
+	//alt_printf("main 1\n");
 	while(1){
-		for (int led = 0; led < 7; led++)
+		//alt_printf("i+ \n");
+		for (int i = 0; i < 7; i++)
 		{
-			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,1<<led);
-			usleep(10000);
+			//alt_printf("i: %x\n",i);
+			led = led<<1;
+			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
+			//alt_printf("led<<1: %x\n",led);
+			usleep(100000);
         }
-        for (int led = 7; led > 0 ; led--)
+		//alt_printf("i- \n");
+        for (int i = 7; i > 0 ; i--)
 		{
-			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,1<<led);
-			usleep(10000);
+			//alt_printf("i: %x\n",i);
+			led = led>>1;
+			IOWR_ALTERA_AVALON_PIO_DATA(LEDS_BASE,led);
+			//alt_printf("led<<1: %x\n",led);
+			usleep(100000);
 		}
 	}
 		
